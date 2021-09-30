7d098e08f8400d4a105fa84f6064353a46a05f28
K0IN <19688162+K0IN@users.noreply.github.com>
2021-09-30 23:38:11 +0200

    Update messages.tsx

--- app/frontend/src/components/messages/messages.tsx
+++ app/frontend/src/components/messages/messages.tsx
@@ -1,5 +1,6 @@
 import { FunctionalComponent, h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
+import { getOfflineDb } from '../../services/messagedb';
 import { MessageType } from '../../types/messagetype';
 import { PushMessage } from '../../types/postmassage';
 
