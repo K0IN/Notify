0ac0d0e04657bfaf92a2ff71797f02d4e4b45537
K0IN <19688162+K0IN@users.noreply.github.com>
2021-11-18 20:56:01 +0100

    Added reload button

--- app/frontend/src/components/register/register.tsx
+++ app/frontend/src/components/register/register.tsx
@@ -6,18 +6,21 @@ import 'preact-material-components/Snackbar/style.css';
 import { useCallback, useRef, useState } from 'preact/hooks';
 import useLogin from '../../hooks/use-login';
 import { useLoginState } from '../../hooks/use-loginstate';
-
+import Button from 'preact-material-components/Button';
+import 'preact-material-components/Button/style.css';
 
 const Register: FunctionalComponent = () => {
     const dialogRef = useRef<Snackbar>();
     const isLoggedIn = useLoginState();
     const setLoginState = useLogin();
     const [loginFailed, setLoginFailed] = useState(false);
+    const [doReload, setReload] = useState(false);
 
     const callback = useCallback(async (e: any) => {
         setLoginFailed(false);
         try {
             await setLoginState(e.target.checked);
+            setReload(true)
         } catch (e: any) {
             dialogRef.current?.MDComponent.show({ message: `unable to register device error: ${e.message}`, timeout: 10000 });
             setLoginFailed(true);
@@ -26,7 +29,8 @@ const Register: FunctionalComponent = () => {
 
     return (
         <div>
-            Subscribe to notifications <Switch onChange={callback} checked={isLoggedIn && !loginFailed} />
+            Subscribe to notifications <Switch onChange={callback} checked={isLoggedIn && !loginFailed} />           
+            {doReload && <Button onClick={() => location.reload()}>reload please</Button>}
             <Snackbar ref={dialogRef} />
         </div>
     );
