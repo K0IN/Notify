8c5e53b7b0b4ad01c85f86ecd9468299a9e5aa88
K0IN <19688162+K0IN@users.noreply.github.com>
2022-01-25 23:04:36 +0100

    Refactored device endpoint
    * Added checks in delete
    * removed background delete of devices
    * formated code and removed unused branches

--- app/src/routes/device.ts
+++ app/src/routes/device.ts
@@ -10,21 +10,17 @@ export const deviceRouter = Router({ base: '/api/device' });
 
 deviceRouter.post('/', authFactory(SERVERPWD),
     async (request: Request): Promise<Response> => {
-        if (!request.json) {
-            return failure({ type: 'missing_data', message: 'body not set' });
-        }
-        
-        const { web_push_data } = await request.json()
-            .catch(() => ({ web_push_data: {}})) as { web_push_data: Partial<WebPushInfos> };
-        
+        const { web_push_data } = await request.json?.()
+            .catch(() => ({ web_push_data: {} })) as { web_push_data: Partial<WebPushInfos> };
+
         if (!web_push_data || !web_push_data.endpoint || !web_push_data.key || !web_push_data.auth) {
             return failure({ type: 'missing_data', message: 'missing web_push_data' }, { status: 400 });
         }
 
         const { endpoint, key, auth } = web_push_data;
 
-        if (String(endpoint).length > 255 
-            || String(key).length > 42 
+        if (String(endpoint).length > 255
+            || String(key).length > 42
             || String(auth).length > 128) {
             return failure({ type: 'invalid_data', message: 'web_push_data member too long' }, { status: 400 });
         }
@@ -48,10 +44,6 @@ deviceRouter.post('/', authFactory(SERVERPWD),
 deviceRouter.get('/:device_id',
     async (request: Required<Request>): Promise<Response> => {
         const { device_id } = request.params as { device_id: string };
-        if (!device_id) {
-            return failure({ type: 'missing_data', message: 'missing device_id' }, { status: 400 });
-        }
-
         const deviceId = String(device_id);
         if (deviceId.length !== 32) {
             return failure({ type: 'invalid_data', message: 'invalid device_id' }, { status: 400 });
@@ -63,13 +55,25 @@ deviceRouter.get('/:device_id',
     });
 
 deviceRouter.delete('/:device_id',
-    async (request: Required<Request>, event: FetchEvent): Promise<Response> => {
+    async (request: Required<Request>): Promise<Response> => {
         const { device_id } = request.params as { device_id: string };
+        const deviceId = String(device_id);
+        if (deviceId.length !== 32) {
+            return failure({ type: 'invalid_data', message: 'invalid device_id' }, { status: 400 });
+        }
+
         const { secret } = await request.json() as { secret: string };
-        return await deleteDevice(String(device_id), String(secret))
-            .then((deletionPromise) => event.waitUntil(deletionPromise))
-            .then(() => success<string>('device deleted'))
-            .catch((error: Error) => failure({ type: 'missing_data', message: error.message }));
+
+        if (!secret) {
+            return failure({ type: 'missing_data', message: 'missing secret' }, { status: 400 });
+        }
+
+        const deviceSecret = String(secret);
+        if (deviceSecret.length !== 32) {
+            return failure({ type: 'invalid_data', message: 'invalid secret' }, { status: 400 });
+        }
+
+        return await deleteDevice(deviceId, deviceSecret)
+            .then(() => success<string>('deleted'))
+            .catch((error: Error) => failure({ type: 'internal_error', message: error.message }));
     });
-
-
