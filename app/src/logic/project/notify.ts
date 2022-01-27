eec3f61e41d2092b4c037580bb538473f737a724
K0IN <19688162+K0IN@users.noreply.github.com>
2022-01-27 20:32:30 +0100

    Added type checks

--- app/src/logic/project/notify.ts
+++ app/src/logic/project/notify.ts
@@ -4,36 +4,31 @@ import { generateWebPushMessage } from '../../webpush/webpush';
 import { WebPushMessage, WebPushResult } from '../../webpush/webpushinfos';
 
 
-export async function notifyAll(title: string, message: string, tags: string[], icon?: string): Promise<Promise<void>> {
+export async function notifyAll(data: string): Promise<Promise<void>> {
     if (!SUB || !VAPID_SERVER_KEY) {
         throw new Error('No Subject or vapid server key please set your secret / env var (see readme)');
     }
 
-    const data = JSON.stringify({ body: message, icon, title, tags });
-
-    if (data.length > 4 * 1024 * 1024) {
-        throw new Error('Message too long');
-    }
-
-    const deviceIds = await getAllDevicesIDs();
     const vapidKeys: Readonly<JWK> = JSON.parse(VAPID_SERVER_KEY);
 
     const webPushMessageInfo: WebPushMessage = {
-        data,
+        data: String(data),
         urgency: 'normal',
         sub: SUB,
         ttl: 60 * 24 * 7
     };
 
-    const promises = deviceIds.map(async (deviceId): Promise<void> => {
+    const deviceIds = await getAllDevicesIDs();
+    const promises = deviceIds.map(async (deviceId): Promise<WebPushResult> => {
         const device = await getDevice(deviceId);
-        if (!device.pushData) {
-            throw new Error('Device has no push data');
+        if (!device || !device.pushData) {
+            return WebPushResult.NotSubscribed;
         }
         const result = await generateWebPushMessage(webPushMessageInfo, device.pushData, vapidKeys);
         if (result === WebPushResult.NotSubscribed) {
             await deleteDeviceFromDatabase(deviceId);
         }
+        return result;
     });
 
     return Promise.all(promises) as unknown as Promise<void>;
