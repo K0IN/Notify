77636365a84a6d416257f76dad428b8c922ded10
K0IN <19688162+K0IN@users.noreply.github.com>
2022-01-28 00:04:02 +0100

    Update full.spec.ts

--- app/test/integrationtests/full.spec.ts
+++ app/test/integrationtests/full.spec.ts
@@ -6,8 +6,8 @@ import { Miniflare } from 'miniflare';
 
 jest.setTimeout(60_000); // 60 seconds timeout
 
-test('responds with url', async () => { 
-    const mf = new Miniflare({  
+test('responds with url', async () => {
+    const mf = new Miniflare({
         envPath: true,
         // packagePath: true,
         wranglerConfigPath: true,
@@ -17,23 +17,23 @@ test('responds with url', async () => {
         kvNamespaces: ['NOTIFY_USERS'],
     });
     const server = mf.startServer();
-    const browser = await pup.launch({ headless: true });
+    const browser = await pup.launch({ headless: false });
     const context = browser.defaultBrowserContext();
     context.overridePermissions('http://localhost:5000', ['notifications']);
- 
+
     const page = await browser.newPage();
     await page.goto('http://localhost:5000');
 
     // click on the button
-    await page.waitForTimeout( 2000 );
+    await page.waitForTimeout(5000);
     await page.screenshot({ path: './images/step_init.png' });
     await page.click('input');
-    await page.waitForTimeout( 2000 );
+    await page.waitForTimeout(5000);
     await page.screenshot({ path: './images/step_clicked.png' });
 
     // reload the page
     await page.reload();
-    await page.waitForTimeout( 2000 );
+    await page.waitForTimeout(5000);
     await page.screenshot({ path: './images/step_reload.png' });
 
     // send a notification
@@ -43,7 +43,7 @@ test('responds with url', async () => {
             message: 'test'
         }),
         method: 'POST'
-    });    
+    });
     expect(res.status).toBe(200);
     const body = await res.json();
     expect(body).toMatchObject({
@@ -52,8 +52,10 @@ test('responds with url', async () => {
         data: expect.any(String)
     });
 
-    // this will throw if we do not receive a notification
+
     await page.waitForXPath('//*[contains(text(), "test")]', { timeout: 5000 });
+
+    // this will throw if we do not receive a notification
     await page.screenshot({ path: './images/step_message.png' });
 
     (await server).close();
