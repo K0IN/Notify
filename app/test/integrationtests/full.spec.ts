8ea08340b399f28f8f65e5932e4ca81341fa6f3c
K0IN <19688162+K0IN@users.noreply.github.com>
2022-01-29 02:31:24 +0100

    Renamend test and removed main branch check

--- app/test/integrationtests/full.spec.ts
+++ app/test/integrationtests/full.spec.ts
@@ -14,28 +14,30 @@ test('responds with url', async () => {
         wranglerConfigPath: true,
         scriptPath: 'dist/index.js',
         port: 5000,
-        executablePath: process.env.PUPPETEER_EXEC_PATH, 
+        executablePath: process.env.PUPPETEER_EXEC_PATH,
         buildCommand: '',
         kvNamespaces: ['NOTIFY_USERS'],
     });
+
     const server = mf.startServer();
     const browser = await pup.launch({ headless: false });
     const context = browser.defaultBrowserContext();
     context.overridePermissions('http://localhost:5000', ['notifications']);
 
-    const page = await browser.newPage();
+    const [page] = await browser.pages();
     await page.goto('http://localhost:5000');
 
     // click on the button
-    await page.waitForTimeout(5000);
+    await page.waitForNetworkIdle({ idleTime: 1000 });
     // await page.screenshot({ path: './images/step_init.png' });
     await page.click('input');
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(10000);
+    //await page.waitForNetworkIdle({ idleTime: 1000 });
     // await page.screenshot({ path: './images/step_clicked.png' });
 
     // reload the page
     await page.reload();
-    await page.waitForTimeout(5000);
+    await page.waitForNetworkIdle({ idleTime: 1000 });
     // await page.screenshot({ path: './images/step_reload.png' });
 
     // send a notification
@@ -55,7 +57,7 @@ test('responds with url', async () => {
     });
 
     // this will throw if we do not receive a notification
-    await page.waitForXPath('//*[contains(text(), "test")]', { timeout: 5000 });
+    await page.waitForXPath('//*[contains(text(), "test")]', { timeout: 60000 });
 
     // await page.screenshot({ path: './images/step_message.png' });
 
