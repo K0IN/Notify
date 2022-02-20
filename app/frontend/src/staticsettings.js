453cd6a5126e93085e3fc70a6c4fc1bf433cede1
K0IN <19688162+K0IN@users.noreply.github.com>
2022-02-20 21:54:03 +0100

    Update staticsettings.js

--- app/frontend/src/staticsettings.js
+++ app/frontend/src/staticsettings.js
@@ -1,4 +1,4 @@
 export const DBNAME = 'notify-db';
 export const DBVERSION = 1;
 // if you want to run the gui separate from the api, change this to to your api location example: "http://localhost:8787/api"
-export const APIBASE = "http://127.0.0.1:8787/api" 
+export const APIBASE = "/api" 
