702920ef5181a6a48f8fa2bbbf5cbecd836909bb
K0IN <19688162+K0IN@users.noreply.github.com>
2022-02-20 21:52:26 +0100

    Updated Added delete and update function for devices

--- app/frontend/src/services/apiservice.ts
+++ app/frontend/src/services/apiservice.ts
@@ -1,14 +1,14 @@
-import { apiBase } from '../staticsettings';
+import { APIBASE } from '../staticsettings';
 import type { IApiResponse } from '../types/apiresponse';
 import type { Device } from '../types/localdevice';
 import type { WebPushData } from '../types/webpushdata';
 
 export async function getVapidData(): Promise<IApiResponse<string>> {
-    return await fetch(`${apiBase}/keys`).then(r => r.json());
+    return await fetch(`${APIBASE}/keys`).then(r => r.json());
 }
 
-export async function createDevice(webPushData: WebPushData, registerToken?: string): Promise<IApiResponse<Device>> {
-    return await fetch(`${apiBase}/device`, {
+export async function createDevice(webPushData: WebPushData, registerToken?: string): Promise<[number, IApiResponse<Device>]> {
+    const res = await fetch(`${APIBASE}/device`, {
         method: 'POST',
         body: JSON.stringify({
             web_push_data: webPushData
@@ -16,9 +16,25 @@ export async function createDevice(webPushData: WebPushData, registerToken?: str
         headers: registerToken ? {
             'Authorization': `Bearer ${registerToken}`
         } : undefined
-    }).then(r => r.json());
+    });
+    return [res.status, await res.json()];
 }
 
 export async function checkIfDeviceExists(deviceId: string): Promise<IApiResponse<boolean>> {
-    return await fetch(`${apiBase}/device/${deviceId}`).then(r => r.json());
+    return await fetch(`${APIBASE}/device/${deviceId}`).then(r => r.json());
+}
+
+export async function deleteDevice(deviceId: string, secret: string): Promise<IApiResponse<string>> {
+    return await fetch(`${APIBASE}/device/${deviceId}`, {
+        method: 'DELETE',
+        body: JSON.stringify({ secret })
+    }).then(r => r.json());
+}
+
+export async function updateDevice(deviceId: string, secret: string, webPushData: WebPushData): Promise<IApiResponse> {
+    const res = await fetch(`${APIBASE}/device/${deviceId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ web_push_data: webPushData, secret })
+    });
+    return await res.json();
 }
