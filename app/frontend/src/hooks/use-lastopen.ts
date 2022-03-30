1e9980cac73cd541afad185b62e894cbddb1bda9
K0IN <thisk0in@gmail.com>
2022-03-30 23:14:32 +0200

    Removed unused variable

--- app/frontend/src/hooks/use-lastopen.ts
+++ app/frontend/src/hooks/use-lastopen.ts
@@ -4,6 +4,6 @@ export const useLastOpenTime = () => {
     const localStorageKey = "lastOpenTime";
     const lastTime = localStorage.getItem(localStorageKey);
     localStorage.setItem(localStorageKey, Date.now().toString());
-    const [lastOpenTime, setLastOpenTime] = useState(Number(lastTime ?? Date.now()));
+    const [lastOpenTime] = useState(Number(lastTime ?? Date.now()));
     return lastOpenTime;
 };
