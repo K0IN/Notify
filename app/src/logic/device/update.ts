39f6da204fa73ef36b443c166233482420374244
K0IN <thisk0in@gmail.com>
2022-06-23 00:23:27 +0200

    Update update.ts

--- app/src/logic/device/update.ts
+++ app/src/logic/device/update.ts
@@ -1,6 +1,6 @@
 import { databaseUpdateDevice } from '../../databases/device';
 import { WebPushInfos } from '../../webpush/webpushinfos';
 
-export async function updateDevice(id: string, secret: string, pushData: WebPushInfos): Promise<void> {  
+export async function updateDevice(id: string, secret: string, pushData: WebPushInfos): Promise<void> {
     await databaseUpdateDevice({ id, secret, pushData });
 }
