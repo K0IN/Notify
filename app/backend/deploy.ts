305e0906a1cd38ec5452d3fcb9b42b69bfd52912
K0IN <19688162+K0IN@users.noreply.github.com>
2024-12-12 21:33:07 +0100

    fix for deno 2 (#180)
    * fix for deno 2
    
    * Fix node version

--- app/backend/deploy.ts
+++ app/backend/deploy.ts
@@ -4,18 +4,18 @@ const values = Deno.env.toObject();
 const port = values.PORT || 80;
 const vapidKey = values.VAPID_KEY || 'eyJrdHkiOiJFQyIsImNydiI6IlAtMjU2IiwiYWxnIjoiRVMyNTYiLCJ4IjoiUV92WlVXUExOUlFMRnU5QWRNaGRDQlFpY1FKamxYajVHZ2lwY19BS1E5USIsInkiOiJILXlDUF9hZ3FzRmpGMmgtZ2dNTTdVT1UxdktJN1JTcU1XSVhfZjBJekhnIiwiZCI6IjVXdzg1TnFxN09lY0pyaDN5MDl6a1JLWWR3TEhUVTVObjlNZUNqMkh6Y2MiLCJrZXlfb3BzIjpbInNpZ24iXSwiZXh0Ijp0cnVlfQ==';
 const sub = values.SUB || 'mailto:example@example.com';
-const cors = values.CORS || false;
+const cors = Boolean(values.CORS) || false;
 const frontend = values.FRONTEND ?? 'static-site';
 const sendkey = values.SENDKEY || undefined;
 const loginkey = values.LOGINKEY || undefined;
 
 
 await serve({
-    port: Number(port),
-    sub: sub,
-    vapidKey: vapidKey,
-    cors: cors,
-    frontend: frontend,
-    sendkey: sendkey,
-    loginkey: loginkey,
+  port: Number(port),
+  sub: sub,
+  vapidKey: vapidKey,
+  cors: cors,
+  frontend: frontend,
+  sendkey: sendkey,
+  loginkey: loginkey,
 })
